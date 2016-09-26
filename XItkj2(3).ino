472b9d8d304c2003adef5340feee09721220b461
adisunanto <Adisoenanto28@gmail.com>
2016-09-25 23:17:57 -0700

    Add files via upload

---
+++ XItkj2(3).ino
@@ -0,0 +1,15 @@
+  // put your setup code here, to run once:
+void setup() { // initialize the digital pin as an output. 
+    pinMode(4, OUTPUT); 
+
+}
+
+  // put your main code here, to run repeatedly:
+  void loop() { 
+    digitalWrite(4, HIGH); // nyalakan LED
+    delay(2500); // tunggu 1000 milidetik
+    digitalWrite(4, LOW); // matikan LED
+    delay(2500); //  tunggu 1000 milidetik  
+
+}
+
